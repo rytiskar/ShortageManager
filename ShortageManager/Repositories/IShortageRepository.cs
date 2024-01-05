f375df35829bd6de89f3c45f1aec8b415d759cd6
Rytis Karalius <rytis.karalius321@gmail.com>
2024-01-05 13:22:56 +0200

    Create ShortageRepository class to interact with data

---
+++ ShortageManager/Repositories/IShortageRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShortageManager.Models;
+
+namespace ShortageManager.Repositories;
+
+public interface IShortageRepository
+{
+    void SaveShortage(Shortage shortage);
+    void DeleteShortage(string title, string room, string userName);
+    List<Shortage> ListShortages(string userName, DateTime? createdOnStart, DateTime? createdOnEnd, string titleFilter, string categoryFilter, string roomFilter);
+
+    // Get all shortages (for administrators)
+    List<Shortage> GetAllShortages();
+    bool ShortageExists(string title, string room);
+    void UpdatePriority(string title, string room, int newPriority);
+}
---
+++ ShortageManager/Repositories/ShortageRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShortageManager.Models;
+using System.Text.Json;
+
+namespace ShortageManager.Repositories;
+
+public class ShortageRepository
+{
+    private readonly string _filepath = @"Data\shortages.json";
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new() { WriteIndented = true };
+
+    void SaveShortage(Shortage shortage)
+    {
+        var shortageJson = JsonSerializer.Serialize(shortage, _jsonSerializerOptions);
+
+        File.WriteAllText(_filepath, shortageJson);
+    }
+
+}
